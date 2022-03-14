13b0205a21685db5e7f61aa5929aed66df92021e
Charles <forson.charlesj@gmail.com>
2022-03-14 20:26:00 +0000

    Updated README

--- README.md
+++ README.md
@@ -4,15 +4,18 @@ Technical challenge to recreate a Figma component in React
 
 ## Assumptions
 
-- Typescript React App created with Create React App
 - Chakra UI Styling Libary used
 - Non-essential dev packages should be in dev dependancies
-- Assumes length of answer to be no more than 30 characters DOES IT?
 - Randomly selects 50% of questions to start correct/ incorrect
-
+- Cannot handle more than two options
 
 ## Next Steps
 
 - Testing
-- Could make it so that multiple choice questions work?
-- Implementing a form libary such as Formik would take away much of the heavy lift
+- Use Grid rather than Flex to allow for more than 2 answers
+- Implementing a form libary such as Formik to allow for multiple questions 
+- Allow multiple choice questions
+
+## Comments 
+
+
--- src/questionData/index.ts
+++ src/questionData/index.ts
@@ -3,22 +3,18 @@ import { Question, SingleChoiceAnswer } from "../Question";
 const q1title =
   "If you had a magic want to change either thing about the world, what would you do?";
 
-const a1data = ["Eat the rich", "Peaceful democatic methods"];
+const a1data = ["Eat the rich", "Persuade the rich to pay tax"];
 
-const answer1 = new SingleChoiceAnswer(a1data, 1);
+const answer1 = new SingleChoiceAnswer(a1data, 0);
 
-const a2data = ["Partially permeable membrane", "Nucleus"];
+const a2data = ["Save the environment", "World peace"];
 
 const answer2 = new SingleChoiceAnswer(a2data, 0);
 
-const a3data = ["Jeff", "Daaaaaave"];
+const a3data = ["End corruption", "Take bribes"];
 
-const answer3 = new SingleChoiceAnswer(a3data, 0);
+const answer3 = new SingleChoiceAnswer(a3data, 1);
 
-const a4data = ["foo", "bar"];
-
-const answer4 = new SingleChoiceAnswer(a4data, 1);
-
-const answers = [answer1, answer2, answer3, answer4];
+const answers = [answer1, answer2, answer3];
 
 export const question1 = new Question(q1title, answers);
